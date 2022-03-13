b7a3bbad4a82b9623696fa8ac2ff19fc24197d89
Samy <dev.exelight@gmail.com>
2022-03-13 22:28:24 +0100

    Styling App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      App
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen pt-36 px-2">
+      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+        <h1>Input</h1>
+        <h1>QR Code</h1>
+        </div>
+      </div>
     </div>
   );
 }
