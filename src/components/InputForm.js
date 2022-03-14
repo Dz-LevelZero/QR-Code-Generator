1b1a292e7ec0477471c87c67df2b5e7e126a36ba
Samy <dev.exelight@gmail.com>
2022-03-14 18:20:41 +0100

    QrCode Component

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -6,6 +6,9 @@ const InputForm = () => {
     <div className="col-span-2 p-6 grid gap-4">
       <InputField />
       <InputColor />
+      <button
+        className="bg-indigo-400 max-w-xs ml-auto px-4 py-2 text-white rounded-sm mt-4 hover:bg-indigo-500 disabled:bg-gray-300"
+      >Generate QrCode</button>
     </div>
   )
 }
