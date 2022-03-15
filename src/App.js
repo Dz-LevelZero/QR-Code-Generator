44cf9921fb40d38fa2f7bd0413c99432fc1f5472
Samy <dev.exelight@gmail.com>
2022-03-15 16:47:03 +0100

    Adding Context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { createContext, useState } from "react";
 import InputForm from "./components/InputForm";
 import QrCode from "./components/QrCode";
 
+// Create Context
+export const InputContext= createContext();
+
 function App() {
+  const [inputValue, setInputValue] = useState({
+    url: "",
+    color: "",
+  })
+  const value = {
+    inputValue,
+    setInputValue,
+  }
+
   return (
     <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen pt-36 px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid md:grid-cols-3">
-          <InputForm />
-          <QrCode />
+          <InputContext.Provider value={value}>
+            <InputForm />
+            <QrCode />
+          </InputContext.Provider>
         </div>
       </div>
     </div>
--- src/components/InputColor.js
+++ src/components/InputColor.js
@@ -1,9 +1,17 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ChromePicker } from "react-color";
+import { InputContext } from "../App";
 
 const InputColor = () => {
   const [color, setColor] = useState('#054080');
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
+
+  // Context
+  const { inputValue, setInputValue } = useContext(InputContext);
+  useEffect(() => {
+    setInputValue({ ...inputValue, color: color });
+  }, [color])
+  
 
   const handleChange = color => setColor(color.hex);
 
